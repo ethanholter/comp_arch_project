ada0a35abace79e2c333353e9084ab5dc3fc9599
Ethan Holter <holterethan@gmail.com>
2026-04-24 17:04:17 -0500

    refactored docs, implemented sisc, reformated ctrl and sisc p3

--- part_3/src/ctrl.v
+++ part_3/src/ctrl.v
@@ -3,203 +3,191 @@
 
 `timescale 1ns/100ps
 
-module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load);
+module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load, rb_sel, mm_sel, dm_we);
 
-  /* Declare the ports listed above as inputs or outputs.  Note that this is
-     only the signals for part 1.  You will be adding signals for parts 2,
-     2, and 4. */
-	  
-	  
-  /* TODO 
-	- wires
-		[ ] br_sel
-		[x] pc_rst
-		[x] pc_write
-		[x] pc_sel
-		[x] ir_load
-	- instructions
-		[ ] BNE
-		[ ] BRR
-		[ ] BNR
-		[ ] HALT
-  */
+/* Declare the ports listed above as inputs or outputs.  Note that this is
+only the signals for part 1.  You will be adding signals for parts 2,
+    2, and 4. */
 
-  input clk, rst_f;
-  input [3:0] opcode, mm, stat;
-  output reg rf_we, wb_sel;
-  output reg [3:0] alu_op;
-  output reg br_sel, pc_rst, pc_write, pc_sel, ir_load;
+   //TODO rb_sel mm_sel dm_we
 
-  // state parameter declarations
+   input clk, rst_f;
+   input [3:0] opcode, mm, stat;
+   output reg rf_we, wb_sel;
+   output reg [3:0] alu_op;
+   output reg br_sel, pc_rst, pc_write, pc_sel, ir_load;
+   output reg rb_sel, mm_sel, dm_we;
 
-  parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
-   
-  // opcode parameter declarations
-  
-  parameter NOOP = 0, REG_OP = 1, REG_IM = 2, SWAP = 3, BRA = 4, BRR = 5, BNE = 6, BNR = 7;
-  parameter JPA = 8, JPR = 9, LOD = 10, STR = 11, CALL = 12, RET = 13, HLT = 15;
-	
-  // addressing modes
-  
-  parameter AM_IMM = 8;
+   // state parameter declarations
 
-  // state register and next state signal
-  
-  reg [2:0]  present_state, next_state;
+   parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
 
-  // initial procedure to initialize the present state to 'start0'.
+   // opcode parameter declarations
 
-  initial
-    present_state = start0;
+   parameter NOOP = 0, REG_OP = 1, REG_IM = 2, SWAP = 3, BRA = 4, BRR = 5, BNE = 6, BNR = 7;
+   parameter JPA = 8, JPR = 9, LOD = 10, STR = 11, CALL = 12, RET = 13, HLT = 15;
 
-  /* Procedure that progresses the fsm to the next state on the positive edge of 
-     the clock, OR resets the state to 'start1' on the negative edge of rst_f. 
-     Notice that the computer is reset when rst_f is low, not high. */
+   // addressing modes
 
-  always @(posedge clk, negedge rst_f)
-  begin
-    if (rst_f == 1'b0)
-      present_state <= start1;
-    else
-      present_state <= next_state;
+   parameter AM_IMM = 8;
+
+   // state register and next state signal
+
+   reg [2:0]  present_state, next_state;
+
+   // initial procedure to initialize the present state to 'start0'.
+
+   initial
+       present_state = start0;
+
+   /* Procedure that progresses the fsm to the next state on the positive edge of 
+   the clock, OR resets the state to 'start1' on the negative edge of rst_f. 
+       Notice that the computer is reset when rst_f is low, not high. */
+
+      always @(posedge clk, negedge rst_f)
+      begin
+          if (rst_f == 1'b0)
+              present_state <= start1;
+          else
+              present_state <= next_state;
+      end
+
+      /* The following combinational procedure determines the next state of the fsm. */
+
+      always @(present_state, rst_f)
+      begin
+          case(present_state)
+              start0:
+                  next_state = start1;
+              start1:
+                  if (rst_f == 1'b0) begin
+                      next_state = start1;
+                      pc_rst = 1;
+                  end
+                  else begin
+                      pc_rst = 0;
+                      next_state = fetch;
+                  end
+                  fetch:
+                      next_state = decode;
+                  decode:
+                      next_state = execute;
+                  execute:
+                      next_state = mem;
+                  mem:
+                      next_state = writeback;
+                  writeback:
+                      next_state = fetch;
+                  default:
+                      next_state = start1;
+              endcase
+          end
+
+          always @(present_state, opcode)
+          begin
+              /* TODO: Generate combinational signals based on the FSM states and inputs. For Parts 2, 3 and 4 you will
+              add the new control signals here. */
+
+             // inputs:   opcode, mm, stat
+             // outputs:  rf_we, wb_sel, alu_op 
+
+             case(present_state)
+
+                 fetch: begin
+                     rf_we <= 0;
+                     ir_load <= 1;
+                     pc_write <= 1;
+
+                     wb_sel <= 0; // for now this always stays off
+                 end
+
+                 decode: begin
+                     ir_load <= 0;
+                     pc_write <= 0;
+
+                     case(opcode)
+                         BRA: begin
+                             br_sel <= 1;
+                             pc_sel <= (mm & stat) != 0;
+                             pc_write <= (mm & stat) != 0;
+                         end
+                         BRR: begin
+                             br_sel <= 0;
+                             pc_sel <= (mm & stat) != 0;
+                             pc_write <= (mm & stat) != 0;
+                         end
+                         BNE: begin
+                             br_sel <= 1;
+                             pc_sel <= (mm & stat) == 0;
+                             pc_write <= (mm & stat) == 0;
+                         end
+                         BNR: begin	
+                         br_sel <= 0;
+                         pc_sel <= (mm & stat) == 0;
+                         pc_write <= (mm & stat) == 0;
+                     end
+                     default: begin end
+                 endcase
+             end
+
+             execute: begin
+                 pc_write <= 0;
+                 br_sel <= 0;
+                 pc_sel <= 0;
+
+                 case(opcode)
+                     REG_OP: begin
+                         alu_op <= 4'b0001;
+                     end
+                     REG_IM: begin
+                         alu_op <= 4'b0011;
+                     end
+                     default: begin 
+                     alu_op <= 4'b0000;
+                 end
+             endcase
+         end
+
+         mem: begin
+             case(opcode)
+                 REG_OP: begin
+                     alu_op <= 4'b0000;
+                 end
+                 REG_IM: begin
+                     alu_op <= 4'b0010;
+                 end
+                 default: begin end
+             endcase
+
+         end
+
+         writeback: begin
+             alu_op <= 4'b0000;
+             case(opcode)
+                 REG_OP: begin
+                     rf_we <= 1;
+                 end
+                 REG_IM: begin
+                     rf_we <= 1;
+                 end
+                 default: begin
+                     rf_we <= 0;
+                 end
+             endcase
+         end
+     endcase
+ end
+
+ // Halt on HLT instruction
+
+ always @ (opcode)
+ begin
+     if (opcode == HLT)
+     begin 
+     #5 $display ("Halt."); //Delay 5 ns so $monitor will print the halt instruction
+     $stop;
+ end
   end
-  
-  /* The following combinational procedure determines the next state of the fsm. */
 
-  always @(present_state, rst_f)
-  begin
-    case(present_state)
-      start0:
-        next_state = start1;
-      start1:
-	  if (rst_f == 1'b0) begin
-			next_state = start1;
-			pc_rst = 1;
-		end
-	 else begin
-			pc_rst = 0;
-         next_state = fetch;
-	 end
-      fetch:
-        next_state = decode;
-      decode:
-        next_state = execute;
-      execute:
-        next_state = mem;
-      mem:
-        next_state = writeback;
-      writeback:
-        next_state = fetch;
-      default:
-        next_state = start1;
-    endcase
-  end
 
-	  always @(present_state, opcode)
-	  begin
-	  /* TODO: Generate combinational signals based on the FSM states and inputs. For Parts 2, 3 and 4 you will
-			 add the new control signals here. */
-		
-		// inputs:   opcode, mm, stat
-		// outputs:  rf_we, wb_sel, alu_op 
-		
-		case(present_state)
-		
-			fetch: begin
-			  rf_we <= 0;
-			  ir_load <= 1;
-			  pc_write <= 1;
-			  
-			  wb_sel <= 0; // for now this always stays off
-			end
-			
-			decode: begin
-				ir_load <= 0;
-				pc_write <= 0;
-
-				case(opcode)
-					BRA: begin
-						br_sel <= 1;
-						pc_sel <= (mm & stat) != 0;
-						pc_write <= (mm & stat) != 0;
-					end
-					BRR: begin
-						br_sel <= 0;
-						pc_sel <= (mm & stat) != 0;
-						pc_write <= (mm & stat) != 0;
-					end
-					BNE: begin
-						br_sel <= 1;
-						pc_sel <= (mm & stat) == 0;
-						pc_write <= (mm & stat) == 0;
-					end
-					BNR: begin	
-						br_sel <= 0;
-						pc_sel <= (mm & stat) == 0;
-						pc_write <= (mm & stat) == 0;
-					end
-					default: begin end
-				endcase
-			end
-			
-			execute: begin
-				pc_write <= 0;
-				br_sel <= 0;
-				pc_sel <= 0;
-				
-				case(opcode)
-					REG_OP: begin
-						alu_op <= 4'b0001;
-					end
-					REG_IM: begin
-						alu_op <= 4'b0011;
-					end
-					default: begin 
-						alu_op <= 4'b0000;
-					end
-				endcase
-			end
-			
-			mem: begin
-				case(opcode)
-					REG_OP: begin
-						alu_op <= 4'b0000;
-					end
-					REG_IM: begin
-						alu_op <= 4'b0010;
-					end
-					default: begin end
-				endcase
-			
-			end
-				
-			writeback: begin
-				alu_op <= 4'b0000;
-				case(opcode)
-					REG_OP: begin
-						rf_we <= 1;
-					end
-					REG_IM: begin
-						rf_we <= 1;
-					end
-					default: begin
-						rf_we <= 0;
-					end
-				endcase
-			end
-		endcase
-	  end
-
-// Halt on HLT instruction
-  
-  always @ (opcode)
-  begin
-    if (opcode == HLT)
-    begin 
-      #5 $display ("Halt."); //Delay 5 ns so $monitor will print the halt instruction
-      $stop;
-    end
-  end
-    
-  
-endmodule
+  endmodule
--- part_3/src/sisc.v
+++ part_3/src/sisc.v
@@ -5,59 +5,79 @@
 
 module sisc (clk, rst_f);
 
-    // inputs
-    input clk, rst_f;
+// inputs
+input clk, rst_f;
 
-    // outputs
+// wires
+wire [31:0] rsa;
+wire [31:0] rsb;
+wire [31:0] alu_result;
+wire [31:0] write_data;
+wire [31:0] instr;
+wire [31:0] read_data;
 
-    // wires
-    wire [31:0] rsa, rsb;
-	 wire [31:0] alu_result;
-	 wire [31:0] write_data;
-	 wire [31:0] instr;
-	 wire [31:0] read_data;
-	 
-    wire [3:0] stat_in, stat_out, stat_en; 									//status register i/o
-    wire rf_we, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load; 	// ctrl module outputs
-	 wire [3:0] alu_op;
-	 
-	 wire [15:0] br_addr;
-	 wire [15:0] pc_out;
-	 wire [15:0] imm;
-	 
-	 wire pc_inc;
-	 
-	 pc PC(clk, br_addr, pc_sel, pc_write, pc_rst, pc_out);
-	 ir u9(clk, ir_load, read_data, instr);
-	 im IM(pc_out, read_data);
-	 br BR(pc_out, instr[15:0], br_sel, br_addr);
-	 
-    ctrl u1 (clk, rst_f, instr[31:28], instr[27:24], stat_out, rf_we, alu_op, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load);
-	 rf u2(clk, instr[19:16], instr[15:12], instr[23:20], write_data, rf_we, rsa, rsb);
-	 alu u3 (clk, rsa, rsb, instr[15:0], stat_out[0], alu_op, instr[27:24], alu_result, stat_in, stat_en);
-	 mux32 u5(alu_result, 0, wb_sel, write_data);
-	 statreg u6(clk, stat_in, stat_en, stat_out);
+wire [15:0] br_addr;
+wire [15:0] pc_out;
+wire [15:0] imm;
+wire [15:0] mem_addr;
 
-	 
-    initial begin
-		$monitor($time,,"IR=%h RSA=%h RSB=%h | R1=%h R2=%h R3=%h R4=%h R5=%h | ALU_OP=%b FUNC=%b STAT=%b | WB_SEL=%h RF_WE=%h WD=%h", 
-			instr,
-			rsa,   
-			rsb,
-			u2.ram_array[1],
-			u2.ram_array[2],
-			u2.ram_array[3],
-			u2.ram_array[4],
-			u2.ram_array[5],
-			alu_op,
-			instr[27:24],
-			stat_out,
-			wb_sel,
-			rf_we,
-			write_data
-		);
-	 end
-	 always @(instr) begin
-		$display("[%h]: %h", pc_out, instr);
-	 end
+wire [3:0] stat_in;
+wire [3:0] stat_out;
+wire [3:0] stat_en; 				//status register i/o
+wire [3:0] alu_op;
+wire [3:0] regb;
+
+wire rf_we;
+wire wb_sel;
+wire br_sel;
+wire pc_rst;
+wire pc_write;
+wire pc_sel;
+wire ir_load; 	// ctrl module outputs
+wire pc_inc;
+wire rb_sel;
+wire mm_sel;
+wire dm_we;
+
+// part 1
+ctrl    u1 (clk, rst_f, instr[31:28], instr[27:24], stat_out, rf_we, alu_op, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load, rb_sel, mm_sel, dm_we);
+rf      u2(clk, instr[19:16], regb, instr[23:20], write_data, rf_we, rsa, rsb);
+alu     u3 (clk, rsa, rsb, instr[15:0], stat_out[0], alu_op, instr[27:24], alu_result, stat_in, stat_en);
+mux32   u5(alu_result, dm_read_data, wb_sel, write_data);
+statreg u6(clk, stat_in, stat_en, stat_out);
+
+// part 2
+br      BR(pc_out, instr[15:0], br_sel, br_addr);
+im      IM(pc_out, read_data);
+ir      u9(clk, ir_load, read_data, instr);
+pc      PC(clk, br_addr, pc_sel, pc_write, pc_rst, pc_out);
+
+// part 3
+dm      u11(mem_addr, mem_addr, rsb, dm_we, dm_read_data);
+mux16   u12(alu_result, instr[15:0], mm_sel, mem_addr);
+mux4    u13(instr[15:12], instr[23:20], rb_sel, regb);
+
+
+
+initial begin
+    $monitor($time,,"IR=%h RSA=%h RSB=%h | R1=%h R2=%h R3=%h R4=%h R5=%h | ALU_OP=%b FUNC=%b STAT=%b | WB_SEL=%h RF_WE=%h WD=%h", 
+	instr,
+	rsa,   
+	rsb,
+	u2.ram_array[1],
+	u2.ram_array[2],
+	u2.ram_array[3],
+	u2.ram_array[4],
+	u2.ram_array[5],
+	alu_op,
+	instr[27:24],
+	stat_out,
+	wb_sel,
+	rf_we,
+	write_data
+    );
+end
+always @(instr) begin
+    $display("[%h]: %h", pc_out, instr);
+end
 endmodule
